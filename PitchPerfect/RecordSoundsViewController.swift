27ff17262610de1ed4bcf8b27887e2af09088fc5
Daniel J Janiak <danieljjaniak@Daniels-MacBook-Air.local>
2016-02-06 20:27:58 -0700

    Remove remaining comments

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -18,17 +18,14 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
     @IBOutlet var userGuide: UILabel!
     
     var audioRecorder:AVAudioRecorder!
-    
     var recordedAudio:RecordedAudio!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -39,17 +36,11 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     @IBAction func recordAudio(sender: UIButton) {
         
-        //Deactivate the record button now that it's been clicked
         recordButton.enabled = false
-        
-        //Deactivate the user guide (label) now that recording is in progress
         userGuide.hidden = true
-        
-        //Make the recording notification and stop button visible
         stopButton.hidden = false
         label.hidden = false
         
-        //Record the user's voice
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
         
         let recordingName = "my_audio.wav"
@@ -101,18 +92,11 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     @IBAction func stopRecord(sender: AnyObject) {
         
-        
-        // Hide the recording label when stop is pressed
         label.hidden = true
-        
-        // Make the user guide visible again
         userGuide.hidden = false
-        
-        // Make the recording process stop.
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
         
-        // Don't forget to stop recording or the app will crash 
         audioRecorder.stop()
 
     }
